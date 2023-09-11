b95657facb56db8fc32e623dabab0bb89822caa2
Gabriel Magallanes <magallanes.gabe@gmail.com>
2023-09-10 23:31:09 -0700

    Added greeting and icon based on time of day

--- Gym Lizard/Screens/StartWorkoutView/StartWorkoutView.swift
+++ Gym Lizard/Screens/StartWorkoutView/StartWorkoutView.swift
@@ -15,9 +15,9 @@ struct Templates: Codable {
 struct StartWorkoutView: View {
     
     //Good morning, good afternoon, etc
-    @State private var greeting = ""
-    @State private var timeOfDayIcon = ""
-    @State private var rotationAngle: Double = 0 // State variable to control the rotation angle of the sun in top right
+    @State private var greeting = "Unknown time. Hello!"
+    @State private var timeOfDayIcon = "questionmark.app.dashed"
+    @State private var timeOfDayColor = Color(.black)
     
     
     let columns: [GridItem] = [GridItem(.flexible()),
@@ -35,18 +35,12 @@ struct StartWorkoutView: View {
                     // Create a background circle using an SF Symbol
                     Image(systemName: timeOfDayIcon)
                         .resizable()
-                        .frame(width: geometry.size.width / 2, height: geometry.size.height / 2)
-                        .foregroundColor(.yellow) // Set the color as needed
-                        .opacity(0.1) // Adjust the opacity as needed
-                        .offset(x: geometry.size.width / 2, y: -geometry.size.height / 3) // Adjust the offset to position the symbol in the top right quarter
-                        .rotationEffect(.degrees(rotationAngle), anchor: .center) // Rotate around the center
-                        .onAppear {
-                            withAnimation(Animation.linear(duration: 10).repeatForever(autoreverses: false)) {
-                                // Animate the rotation
-                                rotationAngle = 360
-                            }
-                        }
+                        .frame(width: geometry.size.width / 1.5, height: geometry.size.height / 2.5)
+                        .foregroundColor(timeOfDayColor)
+                        .opacity(0.2) // Adjust the opacity as needed
+                        .offset(x: geometry.size.width / 2.5, y: -geometry.size.height / 4.5) // Adjust the offset to position the symbol in the top right quarter
                 }
+                
                 ScrollView {
                     VStack (alignment: .leading){
                         
@@ -72,13 +66,16 @@ struct StartWorkoutView: View {
         let hour = Calendar.current.component(.hour, from: Date())
         
         if hour >= 0 && hour < 12 {
-            timeOfDayIcon = "sun.max.fill"
+            timeOfDayIcon = "sun.min"
+            timeOfDayColor = Color(.yellow)
             return "Good Morning"
         } else if hour >= 12 && hour < 17 {
-            timeOfDayIcon = "sun.min"
+            timeOfDayIcon = "sun.max"
+            timeOfDayColor = Color(.orange)
             return "Good Afternoon"
         } else {
-            timeOfDayIcon = "moonphase.waxing.crescent"
+            timeOfDayIcon = "moon.stars"
+            timeOfDayColor = Color(.lightGray)
             return "Good Evening"
         }
     }
