ca7342c9fecd2844a5a985b4eeedecf84e8a9e88
Gabriel Magallanes <magallanes.gabe@gmail.com>
2023-09-11 00:17:28 -0700

    Added truncate the templates if they're too long

--- Gym Lizard/Screens/StartWorkoutView/Subviews/TemplatesSubview.swift
+++ Gym Lizard/Screens/StartWorkoutView/Subviews/TemplatesSubview.swift
@@ -47,34 +47,41 @@ struct TemplateView: View {
     let template: Template
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(template.name)
-                .fontWeight(.bold)
-                .font(.title3)
-                .foregroundColor(Color(.label))
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(
-                            LinearGradient(gradient: Gradient(colors: [Color.orange, Color.green]), startPoint: .leading, endPoint: .trailing),
-                            lineWidth: 2
+        GeometryReader { geometry in
+            ScrollView(.vertical){
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(template.name)
+                        .fontWeight(.bold)
+                        .font(.title3)
+                        .foregroundColor(Color(.label))
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(
+                                    LinearGradient(gradient: Gradient(colors: [Color.orange, Color.green]), startPoint: .leading, endPoint: .trailing),
+                                    lineWidth: 2
+                                )
                         )
-                )
-            Text("Last Performed: \(template.lastPerformed)")
-                .font(.subheadline)
-            ForEach(template.workoutData.keys.sorted(), id: \.self) { workoutTitle in
-                HStack {
-                    Text(workoutTitle)
+                    Text("Last Performed: \(template.lastPerformed)")
                         .font(.subheadline)
-                    Spacer()
-                    Text("Sets: \(template.setsPerWorkout[workoutTitle] ?? 0)")
-                        .font(.subheadline)
+                    ForEach(template.workoutData.keys.sorted(), id: \.self) { workoutTitle in
+                        HStack {
+                            Text(workoutTitle)
+                                .font(.subheadline)
+                            Spacer()
+                            Text("Sets: \(template.setsPerWorkout[workoutTitle] ?? 0)")
+                                .font(.subheadline)
+                        }
+                    }
                 }
+                .padding()
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(10)
+                .frame(minHeight: geometry.size.height) // Set the minimum height to the height of the available space
+                .clipped() // Truncate content exceeding the maximum height
             }
         }
-        .padding()
-        .background(Color.gray.opacity(0.1))
-        .cornerRadius(10)
+        .frame(height: 300) // Set the desired height for truncation
     }
 }
 struct TemplatesSubview_Previews: PreviewProvider {
